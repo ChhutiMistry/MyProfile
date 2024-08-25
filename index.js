f43464aaa8e127b90333156d385518c7884ab0ca
Chhuti Mistry <chhuti246@gmail.com>
2024-08-25 12:18:20 +0530

    Twenty First Change

--- index.js
+++ index.js
@@ -1,85 +1,85 @@
 const validateinfoname = () => {
-    let infoname = document.getElementById('infoname').value;
-    let infodonename = document.getElementById('infodonename');
-    let infoblankname = document.getElementById('infoblankname');
+   let infoname = document.getElementById('infoname').value;
+   let infodonename = document.getElementById('infodonename');
+   let infoblankname = document.getElementById('infoblankname');
 
-    if(infoname.trim().length == 0){
-        infodonename.classList.remove('has-success')
-        infoblankname.innerHTML = "PLEASE FILL IN"
-        infodonename.classList.add('has-error')
-    }else{
-        infoblankname.innerHTML = ""
-        infodonename.classList.remove('has-error')
-        infodonename.classList.add('has-success')
-    }
+   if(infoname.trim().length == 0){
+      infodonename.classList.remove('has-success')
+      infoblankname.innerHTML = "PLEASE FILL IN"
+      infodonename.classList.add('has-error')
+   }else{
+      infoblankname.innerHTML = ""
+      infodonename.classList.remove('has-error')
+      infodonename.classList.add('has-success')
+   }
 }
 
 const validateinfoemail = () => {
-    let infoemail = document.getElementById('infoemail').value;
-    let infodoneemail = document.getElementById('infodoneemail');
-    let infoblankemail = document.getElementById('infoblankemail');
+   let infoemail = document.getElementById('infoemail').value;
+   let infodoneemail = document.getElementById('infodoneemail');
+   let infoblankemail = document.getElementById('infoblankemail');
 
-    if(infoemail.trim().length == 0){
-        infodoneemail.classList.remove('has-success')
-        infoblankemail.innerHTML = "PLEASE FILL IN"
-        infodoneemail.classList.add('has-error')
-    }else{
-        infoblankemail.innerHTML = ""
-        infodoneemail.classList.remove('has-error')
-        infodoneemail.classList.add('has-success')
-    }
+   if(infoemail.trim().length == 0){
+      infodoneemail.classList.remove('has-success')
+      infoblankemail.innerHTML = "PLEASE FILL IN"
+      infodoneemail.classList.add('has-error')
+   }else{
+      infoblankemail.innerHTML = ""
+      infodoneemail.classList.remove('has-error')
+      infodoneemail.classList.add('has-success')
+   }
 }
 
 const validateinfonumber = () => {
-    let infonumber = document.getElementById('infonumber').value;
-    let infodonenumber = document.getElementById('infodonenumber');
-    let infoblanknumber = document.getElementById('infoblanknumber');
+   let infonumber = document.getElementById('infonumber').value;
+   let infodonenumber = document.getElementById('infodonenumber');
+   let infoblanknumber = document.getElementById('infoblanknumber');
 
-    if(infonumber.trim().length == 0){
-        infodonenumber.classList.remove('has-success')
-        infoblanknumber.innerHTML = "PLEASE FILL IN"
-        infodonenumber.classList.add('has-error')
-    }else{
-        infoblanknumber.innerHTML = ""
-        infodonenumber.classList.remove('has-error')
-        infodonenumber.classList.add('has-success')
-    }
+   if(infonumber.trim().length == 0){
+      infodonenumber.classList.remove('has-success')
+      infoblanknumber.innerHTML = "PLEASE FILL IN"
+      infodonenumber.classList.add('has-error')
+   }else{
+      infoblanknumber.innerHTML = ""
+      infodonenumber.classList.remove('has-error')
+      infodonenumber.classList.add('has-success')
+   }
 }
 
 const validateinfodate = () => {
-    let infodate = document.getElementById('infodate').value;
-    let infodonedate = document.getElementById('infodonedate');
-    let infoblankdate = document.getElementById('infoblankdate');
+   let infodate = document.getElementById('infodate').value;
+   let infodonedate = document.getElementById('infodonedate');
+   let infoblankdate = document.getElementById('infoblankdate');
 
-    if(infodate.trim().length == 0){
-        infodonedate.classList.remove('has-success')
-        infoblankdate.innerHTML = "PLEASE FILL IN"
-        infodonedate.classList.add('has-error')
-    }else{
-        infoblankdate.innerHTML = ""
-        infodonedate.classList.remove('has-error')
-        infodonedate.classList.add('has-success')
-    }
+   if(infodate.trim().length == 0){
+      infodonedate.classList.remove('has-success')
+      infoblankdate.innerHTML = "PLEASE FILL IN"
+      infodonedate.classList.add('has-error')
+   }else{
+      infoblankdate.innerHTML = ""
+      infodonedate.classList.remove('has-error')
+      infodonedate.classList.add('has-success')
+   }
 }
 
 const validateinfomessage = () => {
-    let infomessage = document.getElementById('infomessage').value;
-    let infodonemessage = document.getElementById('infodonemessage');
-    let infoblankmessage = document.getElementById('infoblankmessage');
+   let infomessage = document.getElementById('infomessage').value;
+   let infodonemessage = document.getElementById('infodonemessage');
+   let infoblankmessage = document.getElementById('infoblankmessage');
 
-    if(infomessage.trim().length == 0){
-        infoblankmessage.innerHTML = ""
-        infodonemessage.classList.add('has-success')
-    }else{
-        infoblankmessage.innerHTML = ""
-        infodonemessage.classList.add('has-success')
-    }
+   if(infomessage.trim().length == 0){
+      infoblankmessage.innerHTML = ""
+      infodonemessage.classList.add('has-success')
+   }else{
+      infoblankmessage.innerHTML = ""
+      infodonemessage.classList.add('has-success')
+   }
 }
 
 document.onkeydown = function(event) {
-    if (event.keyCode == 123) return false;
-    if (event.ctrlKey && event.shiftKey && event.keyCode == 'I'.charCodeAt(0)) return false;
-    if (event.ctrlKey && event.shiftKey && event.keyCode == 'C'.charCodeAt(0)) return false;
-    if (event.ctrlKey && event.shiftKey && event.keyCode == 'J'.charCodeAt(0)) return false;
-    if (event.ctrlKey && event.keyCode == 'U'.charCodeAt(0)) return false;
-  }
+   if (event.keyCode == 123) return false;
+   if (event.ctrlKey && event.shiftKey && event.keyCode == 'I'.charCodeAt(0)) return false;
+   if (event.ctrlKey && event.shiftKey && event.keyCode == 'C'.charCodeAt(0)) return false;
+   if (event.ctrlKey && event.shiftKey && event.keyCode == 'J'.charCodeAt(0)) return false;
+   if (event.ctrlKey && event.keyCode == 'U'.charCodeAt(0)) return false;
+}
